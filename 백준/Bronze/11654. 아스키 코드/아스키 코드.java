b6484e84cccf270bbda08063ab2f46b3059f8093
jsssun <154240885+jsssun@users.noreply.github.com>
2024-01-11 16:36:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		char ch = in.nextLine().charAt(0);
+		int n = (int)ch;
+		System.out.println(n);
+	}
+}
