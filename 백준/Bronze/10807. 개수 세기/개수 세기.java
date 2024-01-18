cd66ca3f3cb6c610193a8dca4ef6a970e6dfa8c2
jsssun <154240885+jsssun@users.noreply.github.com>
2024-01-18 15:59:28 +0900

    [Bronze V] Title: 개수 세기, Time: 212 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count=0;
+		int N = in.nextInt();
+		int[] arr= new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i]=in.nextInt();
+		}
+		int v = in.nextInt();
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==v) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
